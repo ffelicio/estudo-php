1186de6b8fed06c6a45cbe3aa9b966009e1d1497
ffelicio <fernando_felicio82@yahoo.com.br>
2017-02-12 23:29:06 -0200

    Aplicando o conceito e criação de classes.

---
+++ school-of-net/iniciando-com-oo/orientacao-objetos/index.php
@@ -0,0 +1,9 @@
+<?php
+	require_once('Carro.php');
+
+	/*
+	 * Apesar de utilizar o mesmo molde (Carro) eles ganham a sua indepêndencia.
+	 * Cada um terá as suas características.
+	 */
+	$oFerrari = new Carro();
+	$oMustang = new Carro();
